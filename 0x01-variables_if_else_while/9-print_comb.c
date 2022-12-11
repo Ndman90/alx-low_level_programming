e074da93ab5ac5af478357714e294d0da4667ef2
Ndman90 <nprico99@gmail.com>
2022-12-11 11:13:23 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ * This program prints single base 10 number digits
+ *	separated by comma and space
+ * @num prints single base 10 number digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
