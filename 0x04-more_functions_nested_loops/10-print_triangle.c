33007af86e0616cd6c3a68613d92ca3e36288b84
Ndman90 <onwuamaegbund99@gmail.com>
2023-02-28 15:46:21 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle aligned right, using '#'
+ * @size: Size of triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if ((i + j) <= size)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
