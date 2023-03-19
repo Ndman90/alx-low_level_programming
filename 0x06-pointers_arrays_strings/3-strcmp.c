b1afb3e43f0c9d32981503d3ff7d1100d1f12969
Ndman90 <onwuamaegbund99@gmail.com>
2023-03-18 20:23:20 -0700

    String Comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,8 +14,10 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-		value = s1[i] - s2[i];
-		return (value);
+		{
+			value = s1[i] - s2[i];
+			return (value);
+		}
 	}
 	return (0);
 }
