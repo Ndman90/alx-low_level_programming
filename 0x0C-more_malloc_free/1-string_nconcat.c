7d3cc21ef602fcbd3b0cab6aa29b66a501305a37
Ndman90 <onwuamaegbund99@gmail.com>
2023-08-11 10:03:13 -0700

    a function that concatenates two strings, nth character of the second string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _strlen - calculate and return string length
+ * @str: string
+ * Return: string length
+ */
+int _strlen(char *str)
+{
+	unsigned int i = 0, count = 0;
+
+	while (str[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
  * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
  * @s2: string 2
@@ -11,40 +28,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i = 0, count1 = 0, j = 0, count2 = 0, len1, len2;
-
-	while (s1[i] != '\0')
-	{
-		count1++;
-		i++;
-	}
-	len1 = count1;
-
-	while (s2[j] != '\0')
-	{
-		count2++;
-		j++;
-	}
-	len2 = count2;
+	unsigned int i, j;
 
 	if (s1 == NULL || s2 == NULL)
 		return ("");
-	ptr = malloc(len1 + n * sizeof(int) + 1);
+	ptr = malloc(_strlen(s1) + n * sizeof(int) + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		*(ptr + i) = s1[i];
-	if (n <= len2)
+	if (n <= _strlen(s2))
 	{
 		for (j = 0; j < n; j++)
-			*(ptr + j + len1) = s2[j];
+			*(ptr + j + _strlen(s1)) = s2[j];
 	}
 	else
 	{
 		for (j = 0; s2[j] != '\0'; j++)
-			*(ptr + j + len1) = s2[j];
+			*(ptr + j + _strlen(s1)) = s2[j];
 	}
-	*(ptr + j + len1) = '\0';
+	*(ptr + j + _strlen(s1)) = '\0';
 	return (ptr);
 	free(ptr);
 }
