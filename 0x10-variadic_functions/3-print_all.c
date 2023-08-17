a2f2054d0fc91e8d835013dfe9c8c9876f508687
Ndman90 <onwuamaegbund99@gmail.com>
2023-08-17 10:09:26 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void print_all(const char * const format, ...)
 	separator = "";
 
 	i = 0;
-	while (format != NULL && format[i] != NULL)
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
