7f9c5fc2b96c5b46b2f363569abb9fac8228a4fc
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-16 13:11:32 -0700

    Dynamic library libdynamic.so containing some functions

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char `c`
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
+
